2c2e825f357062256d89c9f7d0a7096907649205
Rong Cui <brookcui97@gmail.com>
2019-12-21 16:45:25 +0800

    Design the article model
    1. Design the article structure with three fields - ID, Title, Content.
    2. To keep the data storage model simple, use global variable articleList to store the list of articles in memory.
    3. Create a function getAllArticles() to get the article list.
    4. Create a function getArticleByID() to find the assumed article by ID and return nil and error if not found.

---
+++ models.article.go
@@ -0,0 +1,27 @@
+package main
+
+import "errors"
+
+type article struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+}
+
+var articleList = []article{
+	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
+	article{ID: 1, Title: "Article 2", Content: "Article 2 body"},
+}
+
+func getAllArticles() []article {
+	return articleList
+}
+
+func getArticleByID(id int) (*article, error) {
+	for _, a := range getAllArticles() {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+	return nil, errors.New("Article not found")
+}
